b6953afeaf663598bf410064574ad508ca8c9c55
Oscar Pettersson <pson93@gmail.com>
2025-05-13 06:49:59 +0200

    push

---
+++ Section-1-Python-Basics-Day-1-App-1-Build-a-List-App-(datatypes)/main.py
@@ -0,0 +1,30 @@
+# todo1 = input("enter item 1: ")
+# todo2 = input("enter item 2: ")
+# todo3 = input("enter item 3: ")
+# todos = [todo1, todo2, todo3]
+
+# print(type(todos))
+
+# print(todos)
+
+# text = input("Enter the length of the book: ")
+# length = len(text)
+
+# print("The length of the book is", length)
+
+# answers = ['Yes', 'No', 'Yes', 'No', 'No']
+# print(answers)
+
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+# my_answer = input("What is your answer?" )
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+my_list = []
+my_list.append("appending")
+my_list.append("appending more")
+print(my_list)
