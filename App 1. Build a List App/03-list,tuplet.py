613f91763a2b4fe54fb17f178b80a1490738f11f
Oscar Pettersson <pson93@gmail.com>
2025-05-17 19:58:55 +0200

    push

--- App 1. Build a List App/03-list,tuplet.py
+++ App 1. Build a List App/03-list,tuplet.py
@@ -9,7 +9,19 @@ for filename in filenames:
  """
  
 
-seconds = [1.23, 1.45, 1.02]
-current = 1.11
+# seconds = [1.23, 1.45, 1.02]
+# current = 1.11
 
-seconds.append(current)
+# seconds.append(current)
+
+# elements = ['a', 'b', 'c']
+# print(elements[1])
+
+# elements = ['a', 'b', 'c']
+# elements[1] = "x"
+# print(elements)
+
+elements = ['a', 'b', 'c']
+new = 'x'
+elements[1] = new
+print(elements)
