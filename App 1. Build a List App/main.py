a7d79b46d6869d57acd2472216d5b661a87262ef
Oscar Pettersson <pson93@gmail.com>
2025-05-18 09:58:24 +0200

    push

--- App 1. Build a List App/main.py
+++ App 1. Build a List App/main.py
@@ -12,11 +12,15 @@ while True:
             todos.append(add_to_list.capitalize())
 
         case "show" | "display":
-            for idx, item in enumerate(todos):
+            for idx, item in enumerate(todos, start = 1):
                 print(f"{idx}: {item}")
         
         case "edit":
-            print(item)
+            index_to_change = int(input("Enter index to change: "))
+            index_to_change = index_to_change - 1
+            new_item = input("Enter new item: ")
+            todos[index_to_change] = new_item.capitalize()
+            print(f"New item: {todos[index_to_change]}")
             #here, again, return functionality
             #to input idx to change
             #then replace that index with new input
