37d8f8d7be6bf50b0806eabbb1af7a869c3f3890
Oscar Pettersson <pson93@gmail.com>
2025-05-21 15:42:01 +0200

    push

--- App 1. Build a List App/bonus6.py
+++
@@ -1,8 +0,0 @@
-countries = ["Albania", "Belgium", "Canada", "Denmark", "Ethiopia", "France"]
-filenames = ['a.txt', 'b.txt', 'c.txt', 'd.txt', 'e.txt', 'f.txt']
-
-for countries, filenames in zip(countries, filenames):
-    file = open(filenames, 'w')
-    file.write(countries)
-    file.close()
-
