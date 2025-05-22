386ca327e6ea0c1f95299fcce6ea6708213e4d2e
Oscar Pettersson <pson93@gmail.com>
2025-05-22 18:29:47 +0200

    push

---
+++ 65.py
@@ -0,0 +1,10 @@
+# Create a program that:
+# 1. generates multiple text files by iterating over the filenames list,
+# 2. and writes the text Hello  inside each generated text file.
+
+filenames = ['doc.txt', 'report.txt', 'presentation.txt']
+
+for filename in filenames:
+    file = open(filename, 'w')
+    file.write("Hello")
+    file.close()
---
+++ 66.py
@@ -0,0 +1,1 @@
+# https://redeploy.udemy.com/course/the-python-mega-course/learn/lecture/38005988#notes
