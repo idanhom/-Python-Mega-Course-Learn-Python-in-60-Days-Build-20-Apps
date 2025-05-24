25f29abe16859429865d81d2055d88ed7523256f
Oscar Pettersson <pson93@gmail.com>
2025-05-24 08:28:24 +0200

    push

--- App 1. Build a List App/main.py
+++ App 1. Build a List App/main.py
@@ -12,36 +12,45 @@ while True:
             file.close()
 
         case "show" | "display":
-            
             file = open('todos.txt', 'r')
             todos = file.readlines()
             file.close()
-            
-
-            # new_todos = [item.strip('\n') for item in todos]
 
             for idx, item in enumerate(todos):
-                # item = item.strip('\n')
-                print(f"{idx + 1}: {item.strip('\n')}")
-        
-
-
-
+                print(f"{idx + 1}: {item.strip()}")
 
         case "edit":
-            index_to_change = int(input("Enter index to change: "))
-            index_to_change = index_to_change - 1
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            index_to_change = int(input("Enter index to change: ")) - 1
             new_item = input("Enter new item: ")
-            todos[index_to_change] = new_item.capitalize()
-            print(f"New item: {todos[index_to_change]}")
+            todos[index_to_change] = new_item.strip() + '\n'
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+
+            print(f"New item: {todos[index_to_change].strip()}")
 
         case "remove":
-            index_to_remove = int(input("Select item to remove: "))
-            print(f"Removed: {todos.pop(index_to_remove - 1)}")
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
 
+            index_to_remove = int(input("Select item to remove: ")) - 1
+            removed_item = todos.pop(index_to_remove)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+
+            print(f"Removed: {removed_item.strip()}")
 
         case "exit":
             break
+
         case _:
             print("wrong input.")
 
