a07ac76042d7d97448bed7d51eaa7c2fae6243e0
Oscar Pettersson <pson93@gmail.com>
2025-05-24 07:57:33 +0200

    push

--- 75.py
+++ 75.py
@@ -1,7 +1,11 @@
 temperatures = [10, 12, 14]
- 
+
 file = open("file.txt", 'w')
 
-file.writelines(temperatures)
+for line in temperatures:
+    file.write(str(line) + '\n')
+    
+
+
 
 
---
+++ 76.py
@@ -0,0 +1,6 @@
+numbers = [10.1, 12.3, 14.7]
+
+numbers = [int(i) for i in numbers]
+print(numbers)
+
+
