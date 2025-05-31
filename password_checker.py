20f8ed1c49fc6ad4638107ad0367622ed5f170db
Oscar Pettersson <pson93@gmail.com>
2025-05-31 07:48:26 +0200

    push

---
+++ password_checker.py
@@ -0,0 +1,23 @@
+# enter while True loop.
+
+while True:
+# declare empty list for password to be stored in, to be checked against
+
+    password_list = []
+
+# enter password, to be stored in above list
+
+    password_list = input("Enter password to check: ")
+
+# start with if-elif-checking
+# if: len(password_list) < 9 then print "insufficient"
+    if len(password_list) < 9:
+        print("Insufficient password")
+
+
+# elif not >= [regex for digit" in password_list: insufficient
+
+    
+
+
+# elif not >= 1 [regex for uppercase] in password_list: insufficient
