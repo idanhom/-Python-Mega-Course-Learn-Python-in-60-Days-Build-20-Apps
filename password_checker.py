baa8459dca220709fca37dc4c90c04daa011f7a4
Oscar Pettersson <pson93@gmail.com>
2025-05-31 09:50:25 +0200

    push

--- password_checker.py
+++ password_checker.py
@@ -14,10 +14,14 @@ while True:
     if len(password_list) < 9:
         print("Insufficient password")
 
+    elif password_list[0].isdigit():
+        
+        # here, we've already confirmed password is longer than 9 char
 
 # elif not >= [regex for digit" in password_list: insufficient
+    elif password_list[0].
+        # now, we're echecking if password_list has digits
 
-    
 
 
 # elif not >= 1 [regex for uppercase] in password_list: insufficient
