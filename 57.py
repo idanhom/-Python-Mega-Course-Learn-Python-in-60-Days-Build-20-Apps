03f9314b4785b0343bef6f7cdaa9a757b4a8d67b
Oscar Pettersson <pson93@gmail.com>
2025-06-01 19:35:04 +0200

    push

---
+++ 57.py
@@ -0,0 +1,10 @@
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# print(letters[1:4])
+
+
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# print(letters[0:3])
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[4:7])
