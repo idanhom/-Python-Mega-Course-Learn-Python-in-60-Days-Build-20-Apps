5cd4c976c938636578521fe698467fcfac5f0f9a
Oscar Pettersson <pson93@gmail.com>
2025-06-12 15:18:11 +0200

    push

--- coding71.py
+++ coding71.py
@@ -13,26 +13,23 @@ def strength(password):
         if char.isupper():
             list_true.append(True)
             break
-        elif char.isupper() != True:
-            list_true.append(False)
-            break
+        else:
+            continue
             
-    # for digit in password:
-    #     if digit.isdigit():
-    #         list_true.append(True)
-    #         break
-    #     elif digit.isdigit() != True:
-    #         list_true.append(False)
-    #         break
-
-
-
-
+    for char in password:
+        if char.isdigit():
+            list_true.append(True)
+            break
+        else:
+            continue
+        
+    if all(list_true) and len(list_true) == 3:
+        print("Password is safe.")
+    else:
+        print("Password is unsafe")
 
     print(list_true)
 
-    if all(list_true):
-        print("Password is safe.")
 
 
 password = input("Enter password to check! ")
