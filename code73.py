db6766fec26f560961d90a671e2b3fa73a8d43a3
Oscar Pettersson <pson93@gmail.com>
2025-06-13 06:11:23 +0200

    push

---
+++ code73.py
@@ -0,0 +1,7 @@
+def greet_person(name):
+    name = name.capitalize()
+    return(f"Hi {name}")
+    
+greet_person("Håkan")
+
+
---
+++ code74.py
@@ -0,0 +1,5 @@
+def concat_strings(str1, str2):
+    concat = str1 + str2
+    return concat
+
+print(concat_strings("Hello", "HEJ"))
