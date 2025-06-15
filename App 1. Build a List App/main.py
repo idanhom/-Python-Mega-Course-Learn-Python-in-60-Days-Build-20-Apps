34d4285469b5bb1dfee603c241f213871051ac7b
Oscar Pettersson <pson93@gmail.com>
2025-06-15 06:47:30 +0200

    push

--- App 1. Build a List App/main.py
+++ App 1. Build a List App/main.py
@@ -1,9 +1,9 @@
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(filepath, todos_list):
+def write_todos(todos_list, filepath='todos.txt'):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_list)
 
@@ -19,15 +19,15 @@ while True:
     if user_input.startswith("add"):
         todo = user_input[4:]
 
-        todos = get_todos('todos.txt')
+        todos = get_todos()
         
         todos.append(todo + '\n')
-        write_todos('todos.txt', todos)
+        write_todos(todos)
 
         
 
     elif user_input.startswith("show") or user_input.startswith("display") or user_input.startswith("ls"):
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
 
         for idx, item in enumerate(todos):
@@ -41,12 +41,12 @@ while True:
         try:
             index_to_change = int(user_input[5:]) - 1 # Slices to number after 'edit' inc. space after. Then subtract 1 since Python does 0-indexing
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
             
             new_item = input("Enter new item: ")
             todos[index_to_change] = new_item.strip() + '\n'
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
             print(f"New item: {todos[index_to_change].strip()}")
         except ValueError:
@@ -57,13 +57,13 @@ while True:
     elif user_input.startswith("remove"):
         try:
                 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
 
             index_to_remove = int(user_input[7:]) - 1
             removed_item = todos.pop(index_to_remove)
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
             print(f"Removed: {removed_item.strip()}")
 
