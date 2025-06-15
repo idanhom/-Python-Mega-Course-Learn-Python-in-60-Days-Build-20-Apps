39f76b522bce546eef696a8215060cd5b4354e7d
Oscar Pettersson <pson93@gmail.com>
2025-06-15 06:54:26 +0200

    push

--- App 1. Build a List App/program/main.py
+++ App 1. Build a List App/program/main.py
@@ -1,13 +1,4 @@
-def get_todos(filepath='todos.txt'):
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(todos_list, filepath='todos.txt'):
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos_list)
-
-
+from functions import get_todos, write_todos
 
 
 while True:
