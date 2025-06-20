187f15ff185300ccf8384345d82041b0aa23edc7
Oscar Pettersson <pson93@gmail.com>
2025-06-20 11:12:41 +0200

    converter gui

---
+++ 155/code155.py
@@ -0,0 +1,13 @@
+# https://img-c.udemycdn.com/redactor/raw/article_lecture/2022-06-17_16-00-50-950c6e645d507334f9ffc30f8ea184a0.png
+
+import FreeSimpleGUI as sg
+
+layout = [
+    [sg.Text("Enter feet:"), sg.Input()],    
+    [sg.Text("Enter inches"), sg.Input()],
+    [sg.Button("Convert")]
+]
+
+window = sg.Window("Convertor", layout)
+window.read()
+window.close()
